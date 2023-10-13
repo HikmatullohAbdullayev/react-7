23e612424c58d9176c40992bc27c7da7cbfa2889
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-10-14 01:49:44 +0500

    btn done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import { Button } from "./components/ul/button"
+import { HomeIcon } from './assets/icon/button-icon.jsx'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <div>
-      <Button>send</Button>
+    <div className='container'>
+      <div>
+        
+         <Button   variant="second" >send</Button> 
+          <Button   variant="frist" >send</Button>
+      </div>
       
     </div>
     </>
--- src/components/ul/button/button.jsx
+++ src/components/ul/button/button.jsx
@@ -16,7 +16,7 @@ export const Button = ({
         className={
           variant === "frist"
             ? style.frist
-            : variant === "scond"
+            : variant === "second"
             ? style.second
             : style.text
         }
