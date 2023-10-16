4cecb017e8bfb3da65752d9893bcd542b8931d97
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-10-17 00:29:02 +0500

    creat done

--- src/pages/post/post.jsx
+++ src/pages/post/post.jsx
@@ -1,11 +1,36 @@
 import React from "react";
-import style from "./post.module.css"
+import { useForm } from "react-hook-form";
+import style from "./post.module.css";
+import {request} from "../../config/requset"
+import { Button } from "../../components/ul/button/button";
 
-export const Post = () =>{
+export const Post = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    return (
-        <>
-        <div>Post</div>
-        </>
-    )
-}
+  const submit =(data) =>{
+    request.post('/posts', data)
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit(submit)}> 
+        <div>
+            <input  {...register('title', {required: true})} type="text" />
+            {errors.title && <p>xato</p>}
+        </div>
+        <div>
+            <input {...register('price', {required: true})} type="text" />
+            {errors.price && <p>xato</p>}
+        </div>
+        <div>
+            <Button type="submit" variant="second"> 
+                post
+            </Button>
+        </div>
+      </form>
+    </>
+  );
+};
